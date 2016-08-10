4fa66a4bce043f19f0585a8ac3a613bdc24378a9
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2016-08-10 11:14:04 -0700

    events_assigment2

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,10 +1,10 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var revealPoint = function (point) {
+var revealPoint = function (points) {
         points.style.opacity = 1;
         points.style.transform = "scaleX(1) translateY(0)";
         points.style.msTransform = "scaleX(1) translateY(0)";
-        points[pointIdx].style.WebkitTransform = "scaleX(1) translateY(0)";
+        points.style.WebkitTransform = "scaleX(1) translateY(0)";
     };
 
 var animatePoints = function (points) {
---
+++ scripts/utilities.js
@@ -0,0 +1,7 @@
+function forEach(arr1, callback)
+{
+    for (var i=0; i < arr1.length; i++)
+    {
+        callback(arr1[i]);    
+    }
+}
