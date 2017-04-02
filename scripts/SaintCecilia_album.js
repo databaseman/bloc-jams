3136e7db9482b292f5c6b4f6d25ff19b6db42071
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2017-04-02 10:38:20 -0700

    added Foo Fighters and Misc Blues

---
+++ scripts/SaintCecilia_album.js
@@ -0,0 +1,336 @@
+var filterTimeCode = function(timeInSeconds)
+{
+   var totalsecs= Math.floor( parseFloat( timeInSeconds ) );
+   var minutes= Math.floor( totalsecs/60 );
+   var seconds= Math.floor( totalsecs-(minutes*60) );
+   return minutes+":" + ( (seconds < 10) ? ("0"+seconds) : seconds);
+}
+
+var seek = function (time) {
+    if (currentSoundFile) {
+        currentSoundFile.setTime(time);
+    }
+}
+
+var setVolume = function (volume) {
+    if (currentSoundFile) {
+        currentSoundFile.setVolume(volume);
+    }
+};
+
+var setSong = function (songNumber) {
+    if (currentSoundFile) {
+        currentSoundFile.stop();
+    }
+
+    currentlyPlayingSongNumber = parseInt(songNumber);
+    currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+    currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+        formats: ['mp3'],
+        preload: true
+    });
+
+    setVolume(currentVolume);
+    
+};
+
+
+
+var getSongNumberCell = function (number) {
+    return $('.song-item-number[data-song-number="' + number + '"]');
+}
+
+var createSongRow = function (songNumber, songName, songLength) {
+    var template =
+        '<tr class="album-view-song-item">' +
+        '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>' +
+        '  <td class="song-item-title">' + songName + '</td>' +
+        '  <td class="song-item-duration">' + filterTimeCode(songLength) + '</td>' +
+        '</tr>';
+    var $row = $(template);
+
+    var clickHandler = function () {
+        var songNumber = parseInt($(this).attr('data-song-number'));
+
+        if (currentlyPlayingSongNumber !== null) {
+            // Revert to song number for currently playing song because user started playing new song.
+            // var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+            var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+            currentlyPlayingCell.html(currentlyPlayingSongNumber);
+        }
+        if (currentlyPlayingSongNumber !== songNumber) {
+            // Switch from Play -> Pause button to indicate new song is playing.
+            setSong(songNumber);
+            currentSoundFile.play();
+            updateSeekBarWhileSongPlays();
+            var $volumeFill = $('.volume .fill');
+            var $volumeThumb = $('.volume .thumb');
+            $volumeFill.width(currentVolume + '%');
+            $volumeThumb.css({
+                left: currentVolume + '%'
+            });
+            $(this).html(pauseButtonTemplate);
+            currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+            updatePlayerBarSong();
+        } else if (currentlyPlayingSongNumber === songNumber) {
+            if (currentSoundFile.isPaused()) {
+                $(this).html(pauseButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPauseButton);
+                currentSoundFile.play();
+            } else {
+                $(this).html(playButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPlayButton);
+                currentSoundFile.pause();
+            }
+        }
+    };
+
+    var onHover = function (event) {
+        var songNumberCell = $(this).find('.song-item-number');
+        var songNumber = parseInt(songNumberCell.attr('data-song-number'));
+
+        if (songNumber !== currentlyPlayingSongNumber) {
+            songNumberCell.html(playButtonTemplate);
+        }
+    };
+
+    var offHover = function (event) {
+        var songNumberCell = $(this).find('.song-item-number');
+        var songNumber = parseInt(songNumberCell.attr('data-song-number'));
+
+        if (songNumber !== currentlyPlayingSongNumber) {
+            songNumberCell.html(songNumber);
+        }
+    };
+
+    $row.find('.song-item-number').click(clickHandler);
+    $row.hover(onHover, offHover);
+    return $row;
+
+};
+
+var setCurrentAlbum = function (album) {
+    currentAlbum = album;
+    var $albumTitle = $('.album-view-title');
+    var $albumArtist = $('.album-view-artist');
+    var $albumReleaseInfo = $('.album-view-release-info');
+    var $albumImage = $('.album-cover-art');
+    var $albumSongList = $('.album-view-song-list');
+    // #2
+    $albumTitle.text(album.title);
+    $albumArtist.text(album.artist);
+    $albumReleaseInfo.text(album.year + ' ' + album.label);
+    $albumImage.attr('src', album.albumArtUrl);
+    $albumSongList.empty();
+
+    for (var i = 0; i < album.songs.length; i++) {
+        var $newRow = createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+        $albumSongList.append($newRow);
+
+    }
+};
+
+var updateSeekBarWhileSongPlays = function () {
+    if (currentSoundFile) {
+        // #10
+        currentSoundFile.bind('timeupdate', function (event) {
+            // #11
+            var seekBarFillRatio = this.getTime() / this.getDuration();
+            var $seekBar = $('.seek-control .seek-bar');
+
+            updateSeekPercentage($seekBar, seekBarFillRatio);
+            setCurrentTimeInPlayerBar(currentSoundFile.getTime());
+        });
+    }
+};
+
+var updateSeekPercentage = function ($seekBar, seekBarFillRatio) {
+    var offsetXPercent = seekBarFillRatio * 100;
+    // #1
+    offsetXPercent = Math.max(0, offsetXPercent);
+    offsetXPercent = Math.min(100, offsetXPercent);
+
+    // #2
+    var percentageString = offsetXPercent + '%';
+    $seekBar.find('.fill').width(percentageString);
+    $seekBar.find('.thumb').css({
+        left: percentageString
+    });
+};
+
+var setupSeekBars = function () {
+    var $seekBars = $('.player-bar .seek-bar');
+
+    $seekBars.click(function (event) {
+        var offsetX = event.pageX - $(this).offset().left;
+        var barWidth = $(this).width();
+        var seekBarFillRatio = offsetX / barWidth;
+
+        if ($(this).parent().attr('class') == 'seek-control') {
+            seek(seekBarFillRatio * currentSoundFile.getDuration());
+        } else {
+            setVolume(seekBarFillRatio * 100);
+        }
+
+        updateSeekPercentage($(this), seekBarFillRatio);
+    });
+
+    $seekBars.find('.thumb').mousedown(function (event) {
+
+        var $seekBar = $(this).parent();
+
+        $(document).bind('mousemove.thumb', function (event) {
+            var offsetX = event.pageX - $seekBar.offset().left;
+            var barWidth = $seekBar.width();
+            var seekBarFillRatio = offsetX / barWidth;
+
+            if ($seekBar.parent().attr('class') == 'seek-control') {
+                seek(seekBarFillRatio * currentSoundFile.getDuration());
+            } else {
+                setVolume(seekBarFillRatio);
+            }
+
+            updateSeekPercentage($seekBar, seekBarFillRatio);
+        });
+
+        // #10
+        $(document).bind('mouseup.thumb', function () {
+            $(document).unbind('mousemove.thumb');
+            $(document).unbind('mouseup.thumb');
+        });
+    });
+};
+
+var trackIndex = function (album, song) {
+    return album.songs.indexOf(song);
+};
+
+var updatePlayerBarSong = function () {
+
+    $('.currently-playing .song-name').text(currentSongFromAlbum.title);
+    $('.currently-playing .artist-name').text(currentAlbum.artist);
+    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
+    $('.main-controls .play-pause').html(playerBarPauseButton);
+    setTotalTimeInPlayerBar(currentSongFromAlbum.duration);
+};
+
+var nextSong = function () {
+
+    var getLastSongNumber = function (index) {
+        return index == 0 ? currentAlbum.songs.length : index;
+    };
+
+    var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+    // Note that we're _incrementing_ the song here
+    currentSongIndex++;
+
+    if (currentSongIndex >= currentAlbum.songs.length) {
+        currentSongIndex = 0;
+    }
+
+    // Set a new current song
+    // currentlyPlayingSongNumber = currentSongIndex + 1;
+    // currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+    setSong(currentSongIndex + 1);
+    currentSoundFile.play();
+    updateSeekBarWhileSongPlays();
+    
+    // Update the Player Bar information
+    updatePlayerBarSong();
+
+    var lastSongNumber = getLastSongNumber(currentSongIndex);
+    // var $nextSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+    // var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+    var $nextSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
+    var $lastSongNumberCell = getSongNumberCell(lastSongNumber);
+
+    $nextSongNumberCell.html(pauseButtonTemplate);
+    $lastSongNumberCell.html(lastSongNumber);
+
+};
+
+var previousSong = function () {
+
+    // Note the difference between this implementation and the one in
+    // nextSong()
+    var getLastSongNumber = function (index) {
+        return index == (currentAlbum.songs.length - 1) ? 1 : index + 2;
+    };
+
+    var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+    // Note that we're _decrementing_ the index here
+    currentSongIndex--;
+
+    if (currentSongIndex < 0) {
+        currentSongIndex = currentAlbum.songs.length - 1;
+    }
+
+    // Set a new current song
+    // currentlyPlayingSongNumber = currentSongIndex + 1;
+    // currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+    setSong(currentSongIndex + 1);
+    currentSoundFile.play(); 
+    updateSeekBarWhileSongPlays();
+    
+    // Update the Player Bar information
+    updatePlayerBarSong();
+
+    var lastSongNumber = getLastSongNumber(currentSongIndex);
+    // var $previousSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+    // var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+    var $previousSongNumberCell = getSongNumberCell(currentlyPlayingSongNumber);
+    var $lastSongNumberCell = getSongNumberCell(lastSongNumber);
+
+    $previousSongNumberCell.html(pauseButtonTemplate);
+    $lastSongNumberCell.html(lastSongNumber);
+
+};
+
+var togglePlayFromPlayerBar = function(e) {
+    // If song is Paused: Play; Update bar to Pause; Update Song cell to Pause; 
+    var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+    if (currentSoundFile.isPaused()) {
+       currentSoundFile.play();       
+       $('.main-controls .play-pause').html(playerBarPauseButton);
+       currentlyPlayingCell.html(pauseButtonTemplate);
+    } else {
+      currentSoundFile.pause();          
+      $('.main-controls .play-pause').html(playerBarPlayButton); 
+      currentlyPlayingCell.html(playButtonTemplate);    
+    } 
+};
+
+var setCurrentTimeInPlayerBar = function( currentTime )
+{
+  var timeFormated= $('.current-time');
+  timeFormated.html( filterTimeCode (currentTime) );
+};
+
+var setTotalTimeInPlayerBar = function( totalTime )
+{   
+   $('.total-time').html(filterTimeCode (totalTime) );
+};
+
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+var playerBarPlayButton = '<span class="ion-play"></span>';
+var playerBarPauseButton = '<span class="ion-pause"></span>';
+
+// Store state of playing songs
+var currentAlbum = null;
+var currentlyPlayingSongNumber = null;
+var currentSongFromAlbum = null;
+var currentSoundFile = null;
+var currentVolume = 80;
+
+var $previousButton = $('.main-controls .previous');
+var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
+
+$(document).ready(function () {
+    setCurrentAlbum(saintCecilia);
+    setupSeekBars();
+    $previousButton.click(previousSong);
+    $nextButton.click(nextSong);
+    $playPauseButton.click(togglePlayFromPlayerBar);
+});
--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -1,17 +1,33 @@
 var bluesMiscArtists = {
     title: 'Blues Album',
     artist: 'Misc Artists',
-    label: 'Unknown',
-    year: '1881',
+    label: 'Misc Lables',
+    year: '',
     albumArtUrl: 'assets/images/album_covers/01.png',
     songs: [
-         { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
-         { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
-         { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
-         { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
-         { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+         { title: 'Hard River to Row (Artist: Delta Generator; Album: Hard River to Row)', duration: 146.4, audioUrl: 'assets/music/bluesMiscArtists/HardRivertoRow' },
+         { title: 'Delta Time (Artist: Hans Theessink & Terry Evans; Album: Delta Time)', duration: 195.6, audioUrl: 'assets/music/bluesMiscArtists/DeltaTime' },
+         { title: 'Going Down To The River (Artist: Saffire-The Uppity Blues Women; Album: Havin The Last Word)', duration: 180, audioUrl: 'assets/music/bluesMiscArtists/GoingDownToTheRiver' },
+         { title: 'Sadie (Artist: Son Seals; Album: Nothing But The Truth)', duration: 364.2, audioUrl: 'assets/music/bluesMiscArtists/Sadie' },
+         { title: 'The Meanderin Hound (Artist: Todd Lorenz; Album: Twenty Aught Eight, V1)', duration: 190.2, audioUrl: 'assets/music/bluesMiscArtists/TheMeanderinHound' }
      ]
 };
+
+var saintCecilia = {
+    title: 'Saint Cecilia EP',
+    artist: 'Foo Fighters',
+    label: 'RCA',
+    year: '2015',
+    albumArtUrl: 'assets/images/album_covers/SaintCecilia.png',
+    songs: [
+         { title: ' Saint Cecilia', duration: 204.6, audioUrl: 'assets/music/bluesMiscArtists/SaintCecilia' },
+         { title: 'Sean', duration: 126.6, audioUrl: 'assets/music/bluesMiscArtists/Sean' },
+         { title: 'Savior Breath', duration: 186.6, audioUrl: 'assets/music/bluesMiscArtists/SaviorBreath' },
+         { title: 'Iron Rooster', duration: 246.6, audioUrl: 'assets/music/bluesMiscArtists/IronRooster' },
+         { title: 'The Neverending Sigh', duration: 267, audioUrl: 'assets/music/bluesMiscArtists/TheNeverendingSigh' }
+     ]
+};
+
 
 var albumPicasso = {
     title: 'The Colors',
