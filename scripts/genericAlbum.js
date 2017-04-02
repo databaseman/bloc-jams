a036da15dc0217dcf5dffcf34fca3238ee75d6e7
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2017-04-02 11:33:32 -0700

    refactor - removing individual album js

--- scripts/genericAlbum.js
+++ scripts/genericAlbum.js
@@ -327,10 +327,3 @@ var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
 var $playPauseButton = $('.main-controls .play-pause');
 
-$(document).ready(function () {
-    setCurrentAlbum(albumPicasso);
-    setupSeekBars();
-    $previousButton.click(previousSong);
-    $nextButton.click(nextSong);
-    $playPauseButton.click(togglePlayFromPlayerBar);
-});
